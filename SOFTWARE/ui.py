2c6aebfc0749d7735dc3af5374908ae0f2c23638
annekil0530 <ykil@caltech.edu>
2020-01-31 15:14:27 -0800

    Update ui.py

--- SOFTWARE/ui.py
+++ SOFTWARE/ui.py
@@ -257,6 +257,7 @@ class MainWindow(QtWidgets.QMainWindow):
         for command in commands[:numfrac]:
             talk(s, command)
             time.sleep(stoptime)
+            break
 
     def pause_resume_pressed(self):
         pass
