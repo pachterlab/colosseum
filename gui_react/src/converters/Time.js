c8ffffbfec7eed1d0351fac5303d895d2e6be840
Anne (Yeokyoung) Kil <ykil@caltech.edu>
2021-03-20 13:01:12 -0700

    Finished time and volume js

--- gui_react/src/converters/Time.js
+++ gui_react/src/converters/Time.js
@@ -1,7 +1,20 @@
 import UnitNumber from './UnitNumber';
 
-const units = [];
-const conversionFactors = [];
+const units = ['sec', 'min', 'hr'];
+const conversionFactors = {
+  'sec' : {
+    'min' : 1/60,
+    'hr'  : 1/3600
+  },
+  'min' : {
+    'sec' : 60,
+    'hr'  : 1/60
+  },
+  'hr' : {
+    'sec' : 3600,
+    'min' : 60
+  }
+};
 
 export class Time extends UnitNumber {
   constructor(value, unit) {
--- gui_react/src/converters/Volume.js
+++ gui_react/src/converters/Volume.js
@@ -1,7 +1,20 @@
 import UnitNumber from './UnitNumber';
 
-const units = [];
-const conversionFactors = [];
+const units = ['uL', 'mL', 'L'];
+const conversionFactors = {
+  'uL' : {
+    'mL' : 0.001,
+    'L'  : 0.000001
+  },
+  'mL' : {
+    'uL' : 1000,
+    'L'  : 0.001
+  },
+  'L' : {
+    'uL' : 1000000,
+    'mL' : 1000
+  }
+};
 
 export class Volume extends UnitNumber {
   constructor(value, unit) {
